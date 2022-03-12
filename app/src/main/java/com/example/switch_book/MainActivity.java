c7e355050f3bf1ed50d4badd661cf2512bb8c5b4
AlanJLJ2202 <lrs19110248@purisima.tecnm.mx>
2022-03-11 19:24:10 -0600

    RegistroActivity

--- app/src/main/java/com/example/switch_book/MainActivity.java
+++ app/src/main/java/com/example/switch_book/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.switch_book;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button btnIngresar;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnIngresar = findViewById(R.id.btnIngresar);
+
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), RegistroActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
 }
