651f01f76db8d0c4b5a3748da70b8d8f7998ca9f
AlanJLJ2202 <lrs19110248@purisima.tecnm.mx>
2022-03-15 17:30:40 -0600

    RespaldoPHP

--- registro.php
+++ registro.php
@@ -1,8 +1,8 @@
 <?php
 $hostname_localhost="localhost";
-$database_localhost="";
-$username_localhost="root";
-$password_localhost="";
+$database_localhost="id18601609_pruebadb";
+$username_localhost="id18601609_root";
+$password_localhost="Switch12345*";
 $json=array();
 
     if(isset($_GET["nombre"]) && isset($_GET["email"]) && isset($_GET["password"])){
@@ -10,16 +10,16 @@ $json=array();
         $email=$_GET['email'];
         $password=$_GET['password'];
 
-        $conexion = mysqli_connect($hostname_localhost, username_localhost, password_localhost, database_localhost);
+        $conexion = mysqli_connect($hostname_localhost, $username_localhost, $password_localhost, $database_localhost);
 
-        $insert="INSERT INTO usuarios(nombre, email, password) VALUES ('{$nombre}','{$email}','{$password}')";
-        $resultado_insert=mysqli_connect($conexion,$insert);
+        $insert="INSERT INTO Usuarios(nombre, email, password) VALUES ('{$nombre}','{$email}','{$password}')";
+        $resultado_insert=mysqli_query($conexion,$insert);
 
         if($resultado_insert){
-            $consulta="SELECT * FROM usuarios WHERE nombre = '{$nombre}'";
+            $consulta="SELECT * FROM Usuarios WHERE nombre = '{$nombre}'";
             $resultado=mysqli_query($conexion, $consulta);
             if($registro=mysqli_fetch_array($resultado)){
-                $json['usuario'][]=$registro;
+                $json['Usuarios'][]=$registro;
             }
             mysqli_close($conexion);
             echo json_encode($json);
@@ -28,7 +28,7 @@ $json=array();
             $resulta["nombre"] = 0;
             $resulta["email"] = 'No registra';
             $resulta["password"] = 'No registra';
-            $json['usuario'][]=$resulta;
+            $json['Usuarios'][]=$resulta;
             echo json_encode($json);
         }
     }
@@ -36,7 +36,7 @@ $json=array();
             $resulta["nombre"] = 0;
             $resulta["email"] = 'WS no retorna';
             $resulta["password"] = 'WS no retorna';
-            $json['usuario'][]=$resulta;
+            $json['Usuarios'][]=$resulta;
             echo json_encode($json);
     }
 
